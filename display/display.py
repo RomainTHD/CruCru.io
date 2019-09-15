ed5a22a7ab4ef037e4cc5d2b06f010c6126b17cb
Rominos111 <39590270+Rominos111@users.noreply.github.com>
2019-09-15 16:34:34 +0300

    Update display.py

--- display/display.py
+++ display/display.py
@@ -1,7 +1,44 @@
 if __name__ == "__main__":
     raise ImportError("Executez main.py")
 
-import pygame as Py
+import pygame
+import time
 
 class Display:
-    pass
+    @classmethod
+    def init(cls, dimension:tuple) -> None:
+        pygame.init()
+
+        cls.width, cls.height = dimension
+
+        cls.window = pygame.display.set_mode((cls.width, cls.height))
+
+        cls.quit = False
+
+        cls.framerate = 60
+
+    @classmethod
+    def bind(cls, key, function):
+        pass
+
+    @classmethod
+    def exit(cls):
+        cls.quit = True
+
+    @classmethod
+    def update(cls):
+        pass
+
+    @classmethod
+    def display(cls):
+
+
+    @classmethod
+    def run(cls):
+        while not cls.quit:
+            cls.update()
+            cls.display()
+
+            pygame.display.flip()
+
+            time.sleep(1/cls.framerate)
