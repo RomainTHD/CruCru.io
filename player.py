e78304ff9055353a1ddbc6890348516cf70149d5
Romain TOD <39590270+Rominos111@users.noreply.github.com>
2019-09-25 17:02:07 +0200

    Add files via upload

--- player.py
+++ player.py
@@ -1,23 +1,33 @@
 from color import Color
 from display import Display
+from map import Map
 
 class Player:
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        self.x = Map.width/2
+        self.y = Map.height/2
 
-        self.radius = 20
+        self.base_radius = 20
+        self.radius = self.base_radius
 
-        self.weight = 0
+        self.score = 0
+
+        self.name = "Player"
 
     def update(self, mx, my):
         coeff = 100
 
-        dist_x = mx - self.x
-        dist_y = my - self.y
+        dist_x = (mx - self.x)/coeff
+        dist_y = (my - self.y)/coeff
 
-        self.x += dist_x / coeff
-        self.y += dist_y / coeff
+        if self.x+dist_x > self.radius and self.x+dist_x < Map.width-self.radius:
+            self.x += dist_x
+
+        if self.y+dist_y > self.radius and self.y+dist_y < Map.height-self.radius:
+            self.y += dist_y
+
+        self.radius = int(self.base_radius + self.score/10)
 
     def display(self):
         Display.drawCircle(x=self.x, y=self.y, color=Color.pink, radius=self.radius)
+        Display.drawText(str(self.score), self.x, self.y, Color.white)
