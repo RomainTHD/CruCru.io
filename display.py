0af9d4a8ed92a577653bb412ce0891261f42605c
Romain TOD <39590270+Rominos111@users.noreply.github.com>
2019-09-25 16:22:25 +0200

    Update display.py

--- display.py
+++ display.py
@@ -1,7 +1,6 @@
 import pygame
 import time
 
-from map import Map
 from color import Color
 
 class Display:
@@ -11,17 +10,21 @@ class Display:
         cls.height = height
         # Largeur et hauteur de la fenêtre
 
+        cls.framerate = 100
+
         cls.window = pygame.display.set_mode((cls.width, cls.height))
 
         pygame.display.set_caption("Agar.io")
         # On change le titre
+
+        cls.font = pygame.font.SysFont("comicsansms", 16)
 
         cls.updateFrame()
         # On actualise la fenêtre
 
     @classmethod
     def updateFrame(cls):
-        time.sleep(1/100)
+        time.sleep(1/cls.framerate)
         # Pour actualiser la fenêtre tous les centième de seconde
 
         pygame.display.flip()
@@ -34,10 +37,10 @@ class Display:
     def drawCircle(cls, x, y, color, radius):
         pygame.draw.circle(cls.window, color, (int(x), int(y)), radius)
 
-
-
-
-
+    @classmethod
+    def drawText(cls, text_str, x, y, color=(255, 255, 255), size=None):
+        text = cls.font.render(text_str, True, color)
+        cls.window.blit(text, (int(x)-text.get_width()//2, int(y)-text.get_height()//2))
 
 # rectangle = pygame.Rect(50, 50, 100, 100)
 # pygame.draw.rect(window, red, rectangle)
