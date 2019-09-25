97a9c1b829d78f3964cfc4792d6d89725d7174d1
Romain TOD <39590270+Rominos111@users.noreply.github.com>
2019-09-25 15:38:36 +0200

    Update color.py

--- color.py
+++ color.py
@@ -1,17 +1,24 @@
+import random
+
 class Color:
     red    = (255, 0  , 0  )
     orange = (255, 127, 0  )
     yellow = (255, 255, 0  )
     lime   = (127, 255, 0  )
     green  = (0  , 255, 0  )
-    # blue1  = (0  , 255, 127)
     cyan   = (0  , 255, 255)
-    # blue2  = (0  , 127, 255)
     blue   = (0  , 0  , 255)
     purple = (127, 0  , 255)
     pink   = (255, 0  , 255)
-    # pink2  = (255, 0  , 127)
 
     black  = (0  , 0  , 0  )
     gray   = (127, 127, 127)
     white  = (255, 255, 255)
+
+    all_color = (red, orange, yellow, lime, green, cyan, blue, purple, pink)
+
+    @classmethod
+    def couleurAleatoire(cls):
+        n = random.randrange(0, len(cls.all_color))
+
+        return cls.all_color[n]
