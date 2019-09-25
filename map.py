320a15160ab8a35c5e77ace2f8fc4a49ffa89d31
Romain TOD <39590270+Rominos111@users.noreply.github.com>
2019-09-25 17:01:53 +0200

    Add files via upload

--- map.py
+++ map.py
@@ -1,4 +1,6 @@
-from random import randrange
+import time
+import random
+
 from boule_a_manger import Boule_a_manger
 
 class Map:
@@ -8,22 +10,34 @@ class Map:
         cls.height = height
         cls.liste_boules=[]
 
+        cls.delta_t = 0.2
+        cls.ref_time = -1*cls.delta_t
+
+        cls.max_cells = 100
+
     @classmethod
     def creer_boule(cls):
-        for i in range(0,5):
-            x = randrange(0, cls.width)
-            y = randrange(0,cls.height)
+        if time.time() - cls.ref_time > cls.delta_t:
+            cls.ref_time = time.time()
+
+            x = random.randrange(0, cls.width)
+            y = random.randrange(0,cls.height)
             b = Boule_a_manger(x,y)
-            if len(cls.liste_boules)<=100 : 
+
+            if len(cls.liste_boules) <= cls.max_cells:
                 cls.liste_boules += [b]
             else :
-                del cls.liste_boules[0:3]
+                del cls.liste_boules[0]
                 cls.liste_boules += [b]
-        
+
     @classmethod
-    def affiche_cellule(cls)
-        for i in range(0,len(liste_boules)):
-            liste_boules[i].display()
-                
-    
-    
+    def affiche_cellule(cls):
+        for i in range(0,len(cls.liste_boules)):
+            cls.liste_boules[i].display()
+
+    @classmethod
+    def hitbox_cellule(cls, player):
+        for i in range(len(cls.liste_boules)-1, -1, -1):
+            if (player.x-cls.liste_boules[i].x)**2 + (player.y-cls.liste_boules[i].y)**2 < (player.radius+cls.liste_boules[i].radius)**2:
+                del cls.liste_boules[i]
+                player.score += 1
