a2a8651c0b553b2d0edd639f0d972c57e0a8b6a9
crumiers <55239926+crumiers@users.noreply.github.com>
2019-10-02 17:02:15 +0200

    Update display.py

--- display.py
+++ display.py
@@ -8,7 +8,16 @@ class Display:
     @classmethod
     def init(cls, width:int=1920, height:int=1080, fullscreen:bool=False, framerate:int=60) -> (int, int):
         """
-
+        Initialisation de l'affichage
+        
+        INPUT :
+            width : int, largeur de la fenêtre
+            height : int, hauteur de la fenêtre
+            fullscreen : bool, plein écran ou non
+            frameRate : int, nombre d'images par seconde
+        
+        OUTPUT :
+            (int, int), largeur et hauteur de la fenêtre 
         """
 
         #! infoObject = pygame.display.Info()
@@ -37,8 +46,18 @@ class Display:
 
     @classmethod
     def updateFrame(cls) -> None:
+        """
+        Procédure pour mettre à jour la fenêtre
+        
+        INPUT :
+            None
+        
+        OUTPUT :
+            None
+        """
+        
         time.sleep(1/cls.framerate)
-        # Pour actualiser la fenêtre tous les centième de seconde
+        # Pour actualiser la fenêtre après un certain temps
 
         pygame.display.flip()
         # On met à jour l'écran
@@ -48,15 +67,40 @@ class Display:
 
     @classmethod
     def drawCircle(cls, pos:Vect2d, color:Color, radius:int) -> None:
+        """
+        Procédure pour afficher un cercle à l'écran
+        
+        INPUT :
+            pos : Vect2d, un vecteur de positions
+            color : Color, une couleur RGB
+            radius : int, le rayon du cercle
+        
+        OUTPUT :
+            None
+        """
+        
         pygame.draw.circle(cls.window, color, (int(pos.x), int(pos.y)), radius)
 
     @classmethod
     def drawText(cls, text_str:str, pos:Vect2d, color:Color=Color.WHITE, size=None) -> None:
+        """
+        Procédure pour afficher du texte à l'écran
+        
+        INPUT :
+            text_str : str, texte à afficher
+            pos : Vect2d, position du texte
+            color : Color, couleur du texte, blanc par défaut
+            size : non utilisé
+        
+        OUTPUT :
+            None
+        """
+        
         text = cls.font.render(text_str, True, color)
         cls.window.blit(text, (int(pos.x)-text.get_width()//2, int(pos.y)-text.get_height()//2))
 
-# rectangle = pygame.Rect(50, 50, 100, 100)
-# pygame.draw.rect(window, red, rectangle)
-# On crée un objet rectangle en (x=50, y=50) de largeur 100 et de hauteur 100
-# ici c'est donc un carré
-# la coordonnée (50, 50) correspond au coin en haut à gauche
+#! rectangle = pygame.Rect(50, 50, 100, 100)
+#! pygame.draw.rect(window, red, rectangle)
+#! On crée un objet rectangle en (x=50, y=50) de largeur 100 et de hauteur 100
+#! ici c'est donc un carré
+#! la coordonnée (50, 50) correspond au coin en haut à gauche
