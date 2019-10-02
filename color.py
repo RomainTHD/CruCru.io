60f8ccf49d764c32d3ff4bf23b7a91d6c9b3c12e
crumiers <55239926+crumiers@users.noreply.github.com>
2019-10-02 16:55:26 +0200

    Update color.py

--- color.py
+++ color.py
@@ -1,6 +1,11 @@
 import random
 
 class Color:
+    """
+    Classe pour gérer l'organisation des couleurs
+    Contient des attributs statiques qui sont des constantes de couleurs
+    """
+    
     RED    = (255, 0  , 0  )
     ORANGE = (255, 127, 0  )
     YELLOW = (255, 255, 0  )
@@ -16,34 +21,46 @@ class Color:
     WHITE  = (255, 255, 255)
 
     @classmethod
-    def randomColor(cls, hue:int=None) -> (int, int, int):
-        if hue is None:
-            hue = random.randrange(0, 360)
+    def randomColor(cls) -> (int, int, int):
+        """
+        Retourne une couleur aléatoire dans l'espace RGB
+        Pour cela, on passe d'abord dans l'espace HSV (teinte, saturation, valeur)
+        Cet espace permet de choisir des couleurs très vives
+
+        INPUT :
+            None
+        
+        OUTPUT :
+            color : tuple de int, un tuple avec les composantes RGB
+        """
+        
+        hue = random.randrange(0, 360)
+        # Teinte aléatoire
 
         color = cls.HSVToRGB(hue, 100, 100)
+        # On passe de HSV à RGB
 
         return color
 
     @staticmethod
     def HSVToRGB(h:int=0, s:int=100, v:int=100) -> (int, int, int):
         """
-        Retourne le code hexadécimal d'une couleur dans l'espace HSV
+        Retourne le code RGB d'une couleur dans l'espace HSV
 
         INPUT :
-            h : int, couleur entre 0 et 360 sur la roue des couleurs
+            h : int, couleur entre 0 et 360
                      0 : rouge, 120 : vert, 260 : bleu, 360 : rouge encore
             s : int, saturation entre 0 et 100, niveau de gris
             v : int, valeur entre 0 et 100, clair ou foncé
 
         OUTPUT :
-            s : tuple, couleur en hexadécimal. Exemple : '#baaaad'
+            s : tuple de int, tuple de couleurs RGB entre 0 et 255
         """
-
-        # Conversion vers RGB
 
         h /= 360
         s /= 100
         v /= 100
+        # Pour avoir des valeurs entre 0 et 1
 
         if s == 0:
             r = g = b = int(v*255)
@@ -72,5 +89,3 @@ class Color:
                 r, g, b = (v, p, q)
 
         return (r, g, b)
-
-#! avoir objet color ?
