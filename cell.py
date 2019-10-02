12d82abe578a588588ecc1cfce9a34cea04ea125
Romain TOD <39590270+Rominos111@users.noreply.github.com>
2019-10-02 16:09:20 +0200

    Update cell.py

--- cell.py
+++ cell.py
@@ -4,13 +4,35 @@ from vector import Vect2d
 
 class Cell:
     """
-    Petite boule inanimée.
+    Petite boule inanimée permettant d'augmenter le score du joueur
     """
 
     def __init__(self, pos:Vect2d) -> None:
+        """
+        Constructeur
+        
+        INPUT :
+            pos : Vect2d, position de la cellule
+           
+        OUTPUT :
+            None
+        """
+        
         self.pos = pos
         self.color = Color.randomColor()
+        # On prend une couleur aléatoire
         self.radius = 5
 
     def display(self) -> None:
-        Display.drawCircle(self.pos, self.color, self.radius)
+        """
+        Procédure d'affichage de la cellule
+        
+        INPUT :
+            None
+        
+        OUTPUT :
+            None
+        """
+        
+        Display.drawCircle(pos=self.pos, color=self.color, radius=self.radius)
+        # Dessine un cercle
