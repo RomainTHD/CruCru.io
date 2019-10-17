86c4772927726a111ad3286a19c6335bd8026e4a
Romain TOD <39590270+Rominos111@users.noreply.github.com>
2019-10-17 07:54:50 +0200

    Ajout IA ennemis - v1

--- enemy.py
+++ enemy.py
@@ -1,6 +1,7 @@
 from display import Display
 from vector import Vect2d
 from creature import Creature
+from cell import Cell
 
 import math
 import random
@@ -9,24 +10,92 @@ class Enemy(Creature):
     def __init__(self, pos, name, color) -> None:
         super().__init__(pos, name, color)
 
-        self.angle = random.random()*math.pi*2
+        self.map = None
+
+        # self.angle = random.random()*math.pi*2
+
+        self.dest_coords = Vect2d((random.random()-0.5)*1000, (random.random()-0.5)*1000)
+        self.dest_score = -1
+
+    def getMapPos(self, width, height, grille_width, grille_height):
+        pos_x = int(self.pos.x/width  * grille_width)
+        pos_y = int(self.pos.y/height * grille_height)
+
+        return pos_x, pos_y
 
     def update(self, width, height):
-        v = Vect2d(math.cos(self.angle), math.sin(self.angle)) * 100
+        maxi = 0
+        liste_pos_maxi = []
 
-        self.angle += 0.05
+        grille_width  = len(self.map)
+        grille_height = len(self.map[0])
+
+        pos_x, pos_y = self.getMapPos(width, height, grille_width, grille_height)
+
+        radius = 1
+
+        for x in range(pos_x-radius, pos_x+radius+1):
+            for y in range(pos_y-radius, pos_y+radius+1):
+                if x in range(grille_width) and y in range(grille_height):
+                    taille = len(self.map[x][y])
+
+                    if taille == maxi:
+                        liste_pos_maxi += [(x, y)]
+                    elif taille > maxi:
+                        maxi = taille
+                        liste_pos_maxi = [(x, y)]
+
+
+
+        if maxi > self.dest_score:
+            self.dest_score = maxi
+
+            distance_mini = float("inf")
+            coords_mini   = None
+
+            for i in range(len(liste_pos_maxi)):
+                x, y = liste_pos_maxi[i]
+
+                for j in range(len(self.map[x][y])):
+                    cell = self.map[x][y][j]
+
+                    dist = Vect2d.dist(cell.pos, self.pos)
+
+                    if dist < distance_mini:
+                        distance_mini = dist
+                        coords_mini = self.map[x][y][j].pos
+
+            if coords_mini is not None:
+                self.dest_coords = coords_mini.copy()
+
+
+
+
+
+
+
+        v = self.dest_coords.copy()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         coeff_tps = 1
-        dist_per_sec = 200
-
-        if v.lengthSq() > self.radius**2:
-            coeff_dist_mouse = 1
-        else:
-            coeff_dist_mouse = v.length()/self.radius
+        dist_per_sec = 100
 
         v = v.normalize()
-
-        v = v*Display.frame_time*dist_per_sec/coeff_tps * coeff_dist_mouse**2
+        v = v*Display.frame_time*dist_per_sec/coeff_tps
 
         max_speed_per_s = 100
 
